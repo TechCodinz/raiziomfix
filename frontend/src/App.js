05f8bae69069195e1a5f698b40ab0c5c72c56f0b
TechCodinz <divyntechcodinz@gmail.com>
2025-07-21 07:32:47 -0700

    Added App.js and updated index.js to fix blank screen

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from RaiziomFix!</h1>
+      <p>This is your deployed frontend.</p>
+    </div>
+  );
+}
+
+export default App;
